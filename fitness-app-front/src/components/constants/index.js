074f13c975a68ae99e431c9d75289961b7ecf715
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-06-07 22:48:25 +0600

    new data

--- fitness-app-front/src/components/constants/index.js
+++ fitness-app-front/src/components/constants/index.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = 'http://localhost:8080';
-export const API_BASE_URL = 'http://plume.fit';
+export const API_BASE_URL = 'http://51.116.236.19:8080';
 export const ACCESS_TOKEN = 'accessToken';
 
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
