e578adfbf8801b6a2251589d314807504795625f
Aika <ATumbayeva@one.kz>
2022-06-15 20:47:57 +0600

    auth

--- fitness-app-front/src/components/constants/index.js
+++ fitness-app-front/src/components/constants/index.js
@@ -3,7 +3,7 @@ export const API_BASE_URL = "http://maria.germanywestcentral.cloudapp.azure.com:
 export const ACCESS_TOKEN = 'accessToken';
 
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
-export const OAUTH2_REDIRECT_URI = 'http://plume.fit:80/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'http://maria.germanywestcentral.cloudapp.azure.com/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
